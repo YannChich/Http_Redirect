63ee03bbcfdc5baee24d0892febfcd6add03b72b
Yann Chicheportiche <118822003+YannChich@users.noreply.github.com>
2023-04-23 17:46:21 +0300

    Start of CC

--- client.py
+++ client.py
@@ -9,27 +9,29 @@ import socket
 import threading
 import time
 
-
-
 # DHCP Server.
 
 client_ip = "0.0.0.0"
 dns_server_ip = "127.0.0.3"
 domain = "example.com"
+resolved_ip = "127.0.0.40"
+connection_open = True
+pause_client = False
 
-#Sending DHCP discover to the server.
+
+# Sending DHCP discover to the server.
 def send_dhcp_dis():
     dhcp_packet = (Ether(dst="ff:ff:ff:ff:ff") /
-                    IP(src='0.0.0.0', dst='255.255.255.255') /
-                    UDP(sport=68, dport=67) /
-                    BOOTP(op=1, chaddr=get_if_raw_hwaddr(conf.iface)[1]) /
-                    DHCP(options=[("message-type", "discover"), "end"]))
+                   IP(src='0.0.0.0', dst='255.255.255.255') /
+                   UDP(sport=68, dport=67) /
+                   BOOTP(op=1, chaddr=get_if_raw_hwaddr(conf.iface)[1]) /
+                   DHCP(options=[("message-type", "discover"), "end"]))
     print("[+] DHCP Discover Sent.")
     sendp(dhcp_packet, verbose=False)
 
 
 # DHCP offer handler function.
-def dhcp_offer(client_ip,packet):
+def dhcp_offer(client_ip, packet):
     if DHCP in packet and packet[DHCP].options[0][1] == 2:
         print("[+] Got A DHCP Offer.")
         client_ip = packet[BOOTP].yiaddr
@@ -43,20 +45,20 @@ def dhcp_offer(client_ip,packet):
                                      ("server_id", packet[IP].src),
                                      "end"]))
         print("[+] DHCP Request Sent.")
-        sendp(dhcp_packet ,verbose=False)
+        sendp(dhcp_packet, verbose=False)
         return client_ip
 
 
 # Applying the actual DHCP offer.
-def got_dhcp_ack(client_ip,packet):
-
+def got_dhcp_ack(client_ip, packet):
     if DHCP in packet and packet[DHCP].options[0][1] == 5:
         print("[+] DHCP ack received.")
         interface_name = conf.iface
         subnet_mask = "255.255.255.0"
         set_ip_command = f"sudo ifconfig {interface_name} {client_ip} netmask {subnet_mask}"
         subprocess.run(set_ip_command, shell=True, check=True)
-        print("[ifconfig] Machine IP set to:",client_ip)
+        print("[ifconfig] Machine IP set to:", client_ip)
+
 
 # DNS request sending.
 def send_dns_query(dns_server_ip, domain, client_ip=None):
@@ -71,6 +73,7 @@ def send_dns_query(dns_server_ip, domain, client_ip=None):
     response = dns.message.from_wire(response_data)
     return response
 
+
 # Extracting the IP from DNS.
 def extract_dns_response_ip(response):
     if response:
@@ -81,43 +84,65 @@ def extract_dns_response_ip(response):
                     return resolved_ip
     return None
 
-# RUDP FUNCTIONCS 
+
+# RUDP FUNCTIONCS
 
 def hostname_to_numeric(hostname):
     return sum(ord(c) for c in hostname)
 
-def handle_lost_packet_signal(client, packet_id,sent_packets):
+
+def handle_lost_packet_signal(client, packet_id, sent_packets):
     packet_data = sent_packets.get(packet_id)
     if packet_data:
+        print("-----------------------------------------------")
         print(f"Resending packet {packet_id}: {packet_data}")
+        print("-----------------------------------------------")
         client.sendto(f"SIGNGET:{packet_id};{packet_data}".encode(), (resolved_ip, 49152))
 
-def receive(server,connection_open,sent_packets):
+
+def receive(server, sent_packets):
+    global connection_open
+    global pause_client
     while True:
         data, addr = server.recvfrom(1024)
         data = data.decode()
         print(f"Received data: {data}")
 
+        if data.startswith("SIGNACK"):
+            print("Receive ACK from the RUDP Server. Connection established.")
+
         if data.startswith("SIGNLOST:"):
             print("Receive SIGNLOST , going to send the lost packet")
             packet_id = int(data.split(":", 1)[1])
             handle_lost_packet_signal(server, packet_id, sent_packets)
+
         elif data.startswith("ACKEND:"):
             print("Received ACKEND from server. Closing connection.")
             connection_open = False
             server.close()
-            sys.exit(0)
+
         elif data.startswith("ECHOREPLY:"):
             packet_id, timestamp = data.split(":", 1)[1].split(";", 1)
-            packet_id = int(packet_id)
             latency = (time.time() - float(timestamp)) * 1000
             print(f"Received ECHOREPLY from server. Latency: {latency:.2f} ms")
 
+        elif data.startswith("STATREPLY:"):
+            packet_id, stats = data.split(":", 1)[1].split(";", 1)
+            print(f"Received STATREPLY from server. Server statistics:\n{stats}")
+
+        elif data.startswith("SIGNFULL"):
+            pause_client = True
+            packet_id = data.split(":", 1)[1].split(";", 1)
+            print("Received SIGNFULL from server. Waiting for window to free up.")
+
+        elif data.startswith("SIGNRDY"):
+            pause_client = False
+            packet_id = data.split(":", 1)[1].split(";", 1)
+            print("Received SIGNRDY from server. Waiting for window to free up.")
 
 
 def RUDP_Client(hostname):
-    connection_open = True
-    resolved_ip = "127.0.0.31"
+    global connection_open
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -128,13 +153,23 @@ def RUDP_Client(hostname):
     sent_packets = {}
     packet_id = hostname_to_numeric(hostname)
     # Ajouter un thread pour écouter les messages entrants du serveur
-    receive_thread = threading.Thread(target=receive, args=(client,connection_open,sent_packets))
+    receive_thread = threading.Thread(target=receive, args=(client, sent_packets))
     receive_thread.daemon = True
     receive_thread.start()
 
     while True:
-        if connection_open == False:
-            create_new_connection = input("Connection closed. Do you want to send SIGNEW to create a new connection? (yes/no): ")
+        if pause_client:
+            for i in range(5):
+                sys.stdout.write('.')
+                sys.stdout.flush()
+                time.sleep(1)
+            sys.stdout.write(
+                '\b \b' * 3)  # Efface les trois points en revenant en arrière et en les remplaçant par des espaces
+            sys.stdout.flush()
+
+        elif not connection_open:
+            create_new_connection = input(
+                "Connection closed. Do you want to send SIGNEW to create a new connection? (yes/no): ")
             if create_new_connection.lower() == "yes":
                 connection_open = True
                 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -143,19 +178,20 @@ def RUDP_Client(hostname):
                 client.sendto(f"SIGNEW:{hostname}".encode(), (resolved_ip, 49152))
             else:
                 print("END OF CONNECTION")
-        
+
         else:
             # input message to let the user what signal is going to be sent
             print("-------------------------------------------------------")
             print("This is the list of signals you can send :")
-            print("SIGNGET : to get the ridirection to the server HTTP")
+            print("SIGNGET : to get the redirection to the server HTTP")
             print("SIGNEND : to end the connection")
             print("SIGNECHO : to know the ping with the server RUDP")
+            print("SIGNSTAT : to know the stat of your connection with the server RUDP")
             print("-------------------------------------------------------")
 
             message = input("SIGNAL :")
 
-            packet_id+=1
+            packet_id += 1
 
             if message == "SIGNGET":
                 print(" [RUDP] Sending a SIGNAL : SIGNGET to the RUDP Server")
@@ -166,22 +202,27 @@ def RUDP_Client(hostname):
                 print(" [RUDP] Sending a SIGNAL : SIGNEND to the RUDP Server")
                 client.sendto(f"SIGNEND:{packet_id};{message}".encode(), (resolved_ip, 49152))
                 sent_packets[packet_id] = message
-            
+
             elif message == "SIGNECHO":
                 timestamp = time.time()
                 print(" [RUDP] Sending a SIGNAL : SIGNECHO to the RUDP Server")
                 client.sendto(f"SIGNECHO:{packet_id};{timestamp}".encode(), (resolved_ip, 49152))
                 sent_packets[packet_id] = message
 
+            elif message == "SIGNSTAT":
+                print(" [RUDP] Sending a SIGNAL : SIGNSTAT to the RUDP Server")
+                client.sendto(f"SIGNSTAT:{packet_id}".encode(), (resolved_ip, 49152))
+                sent_packets[packet_id] = message
+
+
+
 
             else:
                 print("Invalid signal, please try again.")
 
-        
 
 # Main func.
 if __name__ == "__main__":
-
     hostname = input("Enter your name : ")
     # DHCP Block
     # send_dhcp_dis()
@@ -204,8 +245,3 @@ if __name__ == "__main__":
     # print("")
     # RUDP Block
     RUDP_Client(hostname)
-    
-
-
-
-
--- rudproxy.py
+++ rudproxy.py
@@ -3,23 +3,57 @@ import time
 
 SizeOfPacket = 1024
 clients = []
-Server_IP = "127.0.0.31"
+Server_IP = "127.0.0.40"
 last_received_id = {}
+unacked_packets = {}
+client_info = {}
+
 
 def hostname_to_numeric(hostname):
     return sum(ord(c) for c in hostname)
 
-def send_ack(server, addr, name):
-    server.sendto(f"{name} joined ! ACK".encode(), addr)
+
+def send_ack(server, addr):
+    server.sendto("SIGNACK".encode(), addr)
+
 
 def send_lost_packet_signal(server, addr, expected_id):
-    print(f"Sending SIGNLOST:{expected_id} to {addr}")  
+    print(f"Sending SIGNLOST:{expected_id} to {addr}")
     server.sendto(f"SIGNLOST:{expected_id}".encode(), addr)
+
+
+def send_signfull_signal(server, addr):
+    print(f"Sending SIGNFULL to {addr}")
+    server.sendto("SIGNFULL".encode(), addr)
+
+
+def send_signrdy_signal(server, addr):
+    print(f"Sending SIGNRDY to {addr}")
+    server.sendto("SIGNRDY".encode(), addr)
+
+
+def server_statistics(addr):
+    info = client_info.get(addr)
+    if not info:
+        return "No client information available."
+
+    elapsed_time = time.time() - info['connection_time']
+    stats = f"Packets sent: {info['packets_sent']}\nPackets lost: {info['packets_lost']}\nConnected time: {elapsed_time:.2f} seconds"
+    return stats
+
+
+# Congestion Control
+def congestion_control(server, addr):
+    send_signfull_signal(server, addr)
+    # The code for the congestion control
+    send_signrdy_signal(server,addr)
+
 
 
 def remove_client(addr):
     global clients
     clients = [client for client in clients if client[1] != addr]
+
 
 def receive(server):
     global last_received_id
@@ -34,24 +68,37 @@ def receive(server):
                 clients.append((name, addr, numeric_value))
                 last_received_id[addr] = numeric_value
                 print(f"Client : {data} ")
-                send_ack(server, addr, name)
+                client_info[addr] = {'packets_sent': 0, 'packets_lost': 0, 'connection_time': time.time(),
+                                     'congestion_window': 1, 'congestion_threshold': 64}
+                send_ack(server, addr)
 
             elif data.startswith("SIGNGET:"):
                 packet_id, payload = data.split(":", 1)[1].split(";", 1)
                 packet_id = int(packet_id)
+                client_info[addr]['packets_sent'] += 1
+
 
                 if packet_id == last_received_id[addr] + 1:
-                    print(f"Received packet {packet_id} from {addr}: {payload}")
+                    print(f"Received packet {packet_id} GET from {addr}: {payload}")
                     last_received_id[addr] = packet_id
                 else:
                     print(f"Received out of order packet {packet_id} from {addr}: {payload}")
+                    client_info[addr]['packets_lost'] += 1
                     send_lost_packet_signal(server, addr, last_received_id[addr] + 1)
 
             elif data.startswith("SIGNECHO:"):
                 packet_id, timestamp = data.split(":", 1)[1].split(";", 1)
                 packet_id = int(packet_id)
+                client_info[addr]['packets_sent'] += 1
                 print(f"Received SIGNECHO from {addr}. Sending back the timestamp.")
-                server.sendto(f"ECHOREPLY:{packet_id};{timestamp}".encode(), addr)        
+                server.sendto(f"ECHOREPLY:{packet_id};{timestamp}".encode(), addr)
+
+            elif data.startswith("SIGNSTAT:"):
+                packet_id = int(data.split(":", 1)[1])
+                client_info[addr]['packets_sent'] += 1
+                print(f"Received SIGNSTAT from {addr}. Sending server statistics.")
+                stats = server_statistics(addr)
+                server.sendto(f"STATREPLY:{packet_id};{stats}".encode(), addr)
 
             elif data.startswith("SIGNEND:"):
                 packet_id, payload = data.split(":", 1)[1].split(";", 1)
@@ -64,12 +111,14 @@ def receive(server):
         except:
             pass
 
+
 def main():
     print("RUDP Server Running")
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server.bind((Server_IP, 49152))
 
     receive(server)
+
 
 if __name__ == "__main__":
     main()
